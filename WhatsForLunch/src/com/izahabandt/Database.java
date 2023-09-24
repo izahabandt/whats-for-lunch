15c8dec50279660b1c9439c43ecd28e1a5ee724f
izahabandt <izahabandt@gmail.com>
2023-09-24 19:33:20 +0200

    first-commit

---
+++ WhatsForLunch/src/com/izahabandt/Database.java
@@ -0,0 +1,66 @@
+package com.izahabandt;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Database {
+
+    public static final String DB_NAME = "meals.db";
+    public static final String CONNECTION_STRING = "jdbc:sqlite:C:\\Users\\izabe\\Desktop\\JAVA\\WhatsForLunch\\" + DB_NAME;
+
+    private Connection conn;
+    public boolean open() {
+        try {
+            conn = DriverManager.getConnection(CONNECTION_STRING);
+            return true;
+        } catch (SQLException e) {
+            System.out.println("Couldn't connect to database: " + e.getMessage());
+            return false;
+        }
+    }
+    public void close(){
+        try{
+            if(conn != null) {
+                conn.close();
+            }
+        } catch(SQLException e){
+            System.out.println("Couldn't close connection: " + e.getMessage());
+        }
+    }
+
+    public boolean addMeal(Meal meal) {
+        String sql = "INSERT INTO meals (name, description) VALUES (?, ?)";
+        try (PreparedStatement statement = conn.prepareStatement(sql)) {
+            statement.setString(1, meal.getName());
+            statement.setString(2, meal.getDescription());
+            int rowsInserted = statement.executeUpdate();
+            return rowsInserted > 0;
+        } catch (SQLException e) {
+            System.out.println("Error adding meal: " + e.getMessage());
+            return false;
+        }
+    }
+    public List<Meal> getRandomMealsForWeek() {
+        String sql = "SELECT * FROM meals ORDER BY RANDOM() LIMIT 3";
+        List<Meal> meals = new ArrayList<>();
+
+        try (Statement statement = conn.createStatement();
+             ResultSet resultSet = statement.executeQuery(sql)) {
+
+            while (resultSet.next()) {
+                Meal meal = new Meal(
+                        resultSet.getString("name"),
+                        resultSet.getString("description")
+                );
+                meal.setId(resultSet.getInt("id"));
+                meals.add(meal);
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Error selecting random meals: " + e.getMessage());
+        }
+
+        return meals;
+    }
+}
---
+++ WhatsForLunch/src/com/izahabandt/Main.java
@@ -0,0 +1,52 @@
+package com.izahabandt;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Database database = new Database();
+        if (database.open()) {
+            List<Meal> mealsToAdd = new ArrayList<>();
+            mealsToAdd.add(new Meal("Asian Gnocchi", "Gnocchi with tofu and broccoli by Rozkoszny"));
+            mealsToAdd.add(new Meal("Eggplant Lasagne", "Lasagne with eggplant and ricotta cheese by Cookido"));
+            mealsToAdd.add(new Meal("Butter Chicken", "Butter chicken by Cookido"));
+            mealsToAdd.add(new Meal("Korean Tofu", "Rice with tofu and broccoli by Cookido"));
+            mealsToAdd.add(new Meal("Cream Cheese Crepes", "Fried crepes with cream cheese"));
+            mealsToAdd.add(new Meal("Quesadilla", "Quesadilla with tomato sauce, mushrooms and corn"));
+            mealsToAdd.add(new Meal("Gnocchi Salad", "Gnocchi with pesto, mozzarella and tomatoes"));
+            mealsToAdd.add(new Meal("Vegetable Salad", "Vegetable salad with red pepper, cucumber and feta cheese"));
+            mealsToAdd.add(new Meal("Spring Rolls", "Spring rolls with tofu, cucumber and pepper"));
+            mealsToAdd.add(new Meal("Classic Lunch", "Potatoes, cucumber salad and a veggie schnitzel"));
+            mealsToAdd.add(new Meal("Mascarpone Pasta", "Pasta with mascarpone cheese, dried tomatoes, spinach and optionally bacon"));
+
+//            for (Meal meal : mealsToAdd) {
+//                boolean success = database.addMeal(meal);
+//
+//                if (success) {
+//                    System.out.println("Meal added successfully: " + meal.getName());
+//                } else {
+//                    System.out.println("Failed to add meal: " + meal.getName());
+//                }
+//            }
+
+            List<Meal> weeklyMenu = database.getRandomMealsForWeek();
+
+            if (!weeklyMenu.isEmpty()) {
+                System.out.println("Here's your menu for the upcoming week:");
+                for (int i = 0; i < weeklyMenu.size(); i++) {
+                    Meal meal = weeklyMenu.get(i);
+                    System.out.println("Meal " + (i + 1) + ": " + meal.getName());
+                    System.out.println("Description: " + meal.getDescription());
+                    System.out.println();
+                }
+            } else {
+                System.out.println("No meals found in the database.");
+            }
+
+            database.close();
+        } else {
+            System.out.println("Unable to connect to the database.");
+        }
+    }
+}
